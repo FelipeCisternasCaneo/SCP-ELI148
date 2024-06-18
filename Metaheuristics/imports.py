14ce4f05460fd7bd456a9468c7602d8cf5ebc01c
FelipeCisternasCaneo <felipe.cisternas@pucv.cl>
2024-06-18 10:26:56 -0400

    first commit

---
+++ Metaheuristics/imports.py
@@ -0,0 +1,4 @@
+from Metaheuristics.Codes.GWO import iterarGWO
+from Metaheuristics.Codes.WOA import iterarWOA
+from Metaheuristics.Codes.PSO import iterarPSO
+from Metaheuristics.Codes.FOX import iterarFOX
